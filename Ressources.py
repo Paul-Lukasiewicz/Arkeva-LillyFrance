e7da38ce6f9d46b7c2053d217d2eaf931a4fa6a2
paulluka <paul.luka@hotmail.fr>
2024-06-12 19:23:45 +0200

    Création des premiers fichiers et de la logique de connection à la base de donnée, de récupération du modèle d'embedding. Création de la logique de query à la base de donnée

--- Ressources.py
+++ Ressources.py
@@ -1,4 +1,4 @@
-OPENAI_API_KEY = "sk-proj-NOPZAW2GHY44c8FE7TUFT3BlbkFJMJVGeA4NpDLBYDO4kBw1"
+
 
 prompt_template = """
 Vous êtes un assistant dans le secteur de l'industrie. Votre rôle est d'aider les techniciens dans leurs opérations de maintenance.
